a790401f235f47eb097cb9da15bdb30aa9a620a9
saykind <saykind@phystech.edu>
2014-04-14 00:00:10 +0400

    Added stack.s

--- hw-2/stack.s
+++ hw-2/stack.s
@@ -1,4 +1,3 @@
-# White-space means end of line
 	.data
 fmt_d:	.string "%d"
 fmt_c:	.string "%c"
@@ -14,29 +13,77 @@ c:	.byte 0
 	.global main
 	.type main, @function
 main:
+#	 ______  __0x0
+#	:      :
+#	|______|
+#	|______|
+#	|______| __esp
+#	|_%eip_|
+#	:      :
+	pushl	%ebp		# It's not essential to save %ebp value actually
+	movl	%esp, 	%ebp	# But it's one more (and only) step from %eip, which must be untouched	
+#	 ______  __0x0
+#	:      :
+#	|______|
+#	|______|
+#	|______| __esp,	ebp
+#	|_%ebp_| 
+#	|_%eip_|
+#	:      :
 	pushl	%ebp
-	movl	%esp,	%ebp
-	
-	pushl	%ebp
+	pushl	$fmt_xn
+	movl	(%esp),	%eax
+	addl	$4,	%esp
+#	pushl	%eax
+ 	 subl	$4,	%esp
+	 movl	%eax,	(%esp)	
+	call printf	#printf("%x\n", %ebp)
+	addl	$8,	%esp
+
+	movl  -4(%esp),	%eax	# Great argument!
+	pushl	%eax
 	pushl	$fmt_xn
 	call printf
 	addl	$8,	%esp
-	movl	%ebp,	%esp
-	
+
 	pushl	%esp
 	pushl	$fmt_xn
 	call printf
-#	addl	$8,	%esp
-#	movl	%ebp,	%esp
-	
-	movl	4(%esp),	%eax
-	
-	pushl 	%eax
-	pushl 	$fmt_xn
-	call printf	
+	addl	$8,	%esp
 
+	pushl	%ebp
+	pushl	$fmt_d
+	call scanf
+	addl	$8,	%esp
+	
+	movl	(%ebp),	%eax
+	pushl	%eax
+	pushl	$fmt_dn
+	call printf
+	addl	$8,	%esp	
+	
 
 	movl	$0,	%eax
 	movl	%ebp,	%esp
-	popl	%ebp
+#	popl	%ebp
+	 movl	(%esp),	%ebp
+	 addl	$4,	%esp 
+	movl	$54,	%ebp
 	ret
+
+#	 ______ __0x0		 ______
+#	:      :		:      :
+#	:______:		:______:
+#	|______|		|______|
+#	|______|		|______|
+#	|______|		|______|
+#	|______|		|______|
+#	|______|		|______|
+#	|______|		|
+#	|______|		|
+#	|______| __esp		|
+#	|_%eip_|		|
+#	:      :		:
+#	:      :		:
+
+
